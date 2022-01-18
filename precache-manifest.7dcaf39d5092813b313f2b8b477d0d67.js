d0fe352e550fe5b8bd067fffa2263772743af570
Bohdan-Ta <81333397+Bohdan-Ta@users.noreply.github.com>
2022-01-18 23:31:43 +0200

    Updates

---
+++ precache-manifest.7dcaf39d5092813b313f2b8b477d0d67.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "22cfba392e5285e333e138178cee342e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ebbce2b3a3248eac4551",
+    "url": "/static/css/2.1a215020.chunk.css"
+  },
+  {
+    "revision": "0efca97196236373145b",
+    "url": "/static/css/main.20b46f79.chunk.css"
+  },
+  {
+    "revision": "ebbce2b3a3248eac4551",
+    "url": "/static/js/2.54b3ea70.chunk.js"
+  },
+  {
+    "revision": "2d0dc3ea56ea29aa89323d664321ba6d",
+    "url": "/static/js/2.54b3ea70.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0efca97196236373145b",
+    "url": "/static/js/main.36985e21.chunk.js"
+  },
+  {
+    "revision": "007a01cfe53030f441ae",
+    "url": "/static/js/runtime-main.cdfe0e5d.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.c21e5f031eebdd1634dc08f2bda74b32.js"
+  "/precache-manifest.7dcaf39d5092813b313f2b8b477d0d67.js"
 );
 
 self.addEventListener('message', (event) => {
