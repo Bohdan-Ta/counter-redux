a55bc6ae4505dcc49c16d1ae4682ddf13446a316
Bohdan-Ta <81333397+Bohdan-Ta@users.noreply.github.com>
2022-01-18 18:27:53 +0200

    Updates

---
+++ precache-manifest.c21e5f031eebdd1634dc08f2bda74b32.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f8e833978b0cf1b0d2fff8f24f43a507",
+    "url": "/index.html"
+  },
+  {
+    "revision": "659c71a6c81585008e92",
+    "url": "/static/css/2.1a215020.chunk.css"
+  },
+  {
+    "revision": "aeae2f84244a38d165b8",
+    "url": "/static/css/main.0f518976.chunk.css"
+  },
+  {
+    "revision": "659c71a6c81585008e92",
+    "url": "/static/js/2.25660725.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/static/js/2.25660725.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "aeae2f84244a38d165b8",
+    "url": "/static/js/main.1564fc1c.chunk.js"
+  },
+  {
+    "revision": "007a01cfe53030f441ae",
+    "url": "/static/js/runtime-main.cdfe0e5d.js"
+  }
+]);
